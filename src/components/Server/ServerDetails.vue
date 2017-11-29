ad33e656ca1eea5aa54fa35d19f608b00d38dc5d
trevor glick <trevor@onprem.com>
2017-11-29 00:02:25 -0600

    submission for exercise7

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="selectedServer === ''">Server Details are currently not updated</p>
+        <div v-else>
+            <p>Selected Server</p>
+            <div class='serverId'>ID# {{ selectedServer.id }}</div>
+            <div class='serverStatus'>Status: {{ selectedServer.status }}</div>
+            <button @click="changeStatus">Change Status</button>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { eventBus } from '../../main'
+    export default {
+        data: function() {
+            return {
+                selectedServer: ''
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.selectedServer.status = 'Normal'
+            }
+        },
+        created() {
+            eventBus.$on('serverWasSelected', (server) => {
+                this.selectedServer = server
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li @click="getDetails(server)"
+                    class="list-group-item"
+                    v-for="server in servers">
+                Server #{{ server.id }} : Status: {{ server.status }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { eventBus } from '../../main'
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    { id: 1, status: "Normal"},
+                    { id: 2, status: "Critical"},
+                    { id: 3, status: "Unknown"},
+                    { id: 4, status: "Normal"},
+                    { id: 5, status: "Down"}
+                ]
+            }
+        },
+        methods: {
+            getDetails(selection) {
+                this.server = selection
+                eventBus.serverSelected(selection)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
